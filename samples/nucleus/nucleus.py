9b4ad5458967106f07beda9600c5a220044fc67f
Sabyasachi Paul <sabya.pl@gmail.com>
2018-04-15 22:02:41 +0200

    Update nucleus.py

--- samples/nucleus/nucleus.py
+++ samples/nucleus/nucleus.py
@@ -111,7 +111,7 @@ class NucleusConfig(Config):
     NUM_CLASSES = 1 + 1  # Background + nucleus
 
     # Number of training and validation steps per epoch
-    STEPS_PER_EPOCH = (664 - len(VAL_IMAGE_IDS)) // IMAGES_PER_GPU
+    STEPS_PER_EPOCH = (664 + 65 - len(VAL_IMAGE_IDS)) // IMAGES_PER_GPU
     VALIDATION_STEPS = max(1, len(VAL_IMAGE_IDS) // IMAGES_PER_GPU)
 
     # Don't exclude based on confidence. Since we have two classes
